29ea058ef3b6e788940a670091a1c142e5dc4583
Gokulan <gokulan1711@gmail.com>
2023-08-20 13:52:03 +0530

    tomcat setup aug20

--- src/main/java/com/restApi/Inventory/InventoryApplication.java
+++ src/main/java/com/restApi/Inventory/InventoryApplication.java
@@ -2,13 +2,20 @@ package com.restApi.Inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InventoryApplication {
+public class InventoryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 		System.out.println("Hi You Are Connected");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(InventoryApplication.class);
 	}
 
 }
